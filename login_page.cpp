e2d523d294f88575305cc6aed698ecf3f936df56
Naman Gupta <namangupta16@gmail.com>
2021-07-21 16:36:47 +0530

    Added Change Password Functionality

--- login_page.cpp
+++ login_page.cpp
@@ -1,6 +1,7 @@
 #include "headers.cpp"
 #include "pwd_getter.cpp"
 using namespace std;
+void change_pass(string, string);
 
 void login_page(string Iuser_name, string Iuser_pwd){
 	int num = 0;
@@ -10,21 +11,58 @@ void login_page(string Iuser_name, string Iuser_pwd){
 		cin >> num;
 		if(num == 1)
 			return;
-		else if(num == 2){
-			string user_name;
-			string user_pwd;
-			fstream users("user.txt");
-			users >> user_name;
-			while(user_name != Iuser_name){
-				users >> user_pwd >> user_name;
-			}
-			users.unget();
-			cout << "Enter Password: ";
-			user_pwd = pwd_getter();
-			users << user_pwd << endl;
-			users.close();
-			system("pause");
-		}
+		else if(num == 2)
+			change_pass(Iuser_name, Iuser_pwd);
 	}
 	return;
 }
+
+void change_pass(string user_name, string user_pwd){
+	system("cls");
+	string current_pass;
+	string new_pass, new_pass_re;
+	cout << "Please Enter Your Current Password:\n";
+	current_pass = pwd_getter();
+	cout << "Please Enter Your New Password:\n";
+	new_pass = pwd_getter();
+	cout << "Please Re-Enter Your New password:\n";
+	new_pass_re = pwd_getter();
+	if(current_pass != user_pwd){
+		cout << "\nYour Current Password is wrong!!\n";
+		system("pause");
+	}
+	else if(new_pass != new_pass_re){
+		cout << "\nYour Passwords don\'t match\n";
+		system("pause");
+	}
+	else{
+		ifstream users("user.txt");
+		ofstream temp("temp.txt");
+		string user_nameFFile, user_pwdFFile;
+		users >> user_nameFFile >> user_pwdFFile;
+		while(!users.eof()){
+			if(user_nameFFile == user_name)
+				temp << user_name << " " << new_pass << endl;
+			else
+				temp << user_nameFFile << " " << user_pwdFFile << endl;
+			users >> user_nameFFile >> user_pwdFFile;
+		}
+		users.close();
+		temp.close();
+		remove("user.txt");
+		rename("temp.txt", "user.txt");
+		cout << "\nPassword Changed Successfully\n";
+		system("pause");
+	}
+	return;
+}
+
+
+
+
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,86 +1,8 @@
 #include "headers.cpp"
-#include "login_page.cpp"
+#include "start_page.cpp"
 using namespace std;
-string set_uname();
-string set_upwd();
-
-void start(){
-	int num = 0;
-	while(num != 3){
-		cout << "1. New User?\n2. Login\n3. Exit\nEnter: ";
-		cin >> num;
-		system("cls");
-		if(num == 1){
-			ofstream users("user.txt", ios::app);
-			string user_name = set_uname();
-			string user_pwd = set_upwd();
-			system("cls");
-			users << user_name << " " << user_pwd << endl;
-			cout << "Registration Done.";
-			users.close();
-		}
-		else if(num == 2){
-			string Iuser_name, Iuser_pass;
-			int found = 0;
-			cout << "Enter Username: ";
-			cin >> Iuser_name;
-			Iuser_pass = pwd_getter();
-			ifstream users("user.txt");
-			string user_name, user_pwd;
-			while(found == 0 && !users.eof()){
-				users >> user_name >> user_pwd;
-				if(user_name == Iuser_name){
-					if(user_pwd == Iuser_pass){
-						cout << "Login Successful\n";
-						found = 1;
-						system("pause");
-					}
-					else{
-						cout << "Wrong Password. Login Failed\n";
-						found = 2;
-					}
-				}
-			}
-			users.close();
-			if(found == 0)
-				cout << "User Not Found\n";
-			else if(found == 1)
-				login_page(user_name, user_pwd);
-			system("pause");
-		}
-		system("cls");
-		if(num == 3)
-			cout << "\nThanks\n";
-	}
-}
 
 int main(){
 	start();
 	return 0;
-}
-
-string set_uname(){
-	string u_name;
-	cout << "Enter Username(without spaces): ";
-	cin >> u_name;
-	return u_name;
-}
-	
-string set_upwd(){
-	string u_pwd;
-	system("cls");
-	int pass_set = 0;
-	string pass = "";
-	cout << "Minimum Length of Password should be 8 and Use of special character(@) is Recommended For a strong Password\n";
-	while(pass_set != 1){
-		pass = pwd_getter();
-		if(pass.size() >= 8)
-			pass_set = 1;
-		else
-			cout << "Minimum Length Should Be 8. Pls Re-Enter\n";
-	}
-	u_pwd = pass;
-	cout << "Password Accepted\n";
-	system("pause");
-	return u_pwd;
 }
